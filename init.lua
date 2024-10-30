c29c4063334777863560267574478be58d49b2c8
Ian Chandler <ian.b.chandler@rakuten.com>
2024-10-30 16:14:18 +0900

    Added vim-test

--- init.lua
+++ init.lua
@@ -933,13 +933,25 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   {
-    "christoomey/vim-tmux-navigator",
+    'christoomey/vim-tmux-navigator',
     vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
     vim.keymap.set('n', 'C-j', ':TmuxNavigateDown<CR>'),
     vim.keymap.set('n', 'C-k', ':TmuxNavigateUp<CR>'),
     vim.keymap.set('n', 'C-l', ':TmuxNavigateRight<CR>'),
   },
 
+  {
+    'vim-test/vim-test',
+    dependencies = {
+      'preservim/vimux',
+    },
+    vim.keymap.set('n', '<leader>t', ':TestNearest<CR>'),
+    vim.keymap.set('n', '<leader>T', ':TestFile<CR>'),
+    vim.keymap.set('n', '<leader>a', ':TestSuite<CR>'),
+    vim.keymap.set('n', '<leader>l', ':TestLast<CR>'),
+    vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
+    vim.cmd "let test#strategy = 'vimux'",
+  },
 
   {
     'MeanderingProgrammer/render-markdown.nvim',
